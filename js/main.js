fef7ed5872ae2933945dbcf36ee7a8c581e39784
alessio <alessio.cirillo.1995@gmail.com>
2020-11-06 15:26:56 +0100

    VueJs trainig

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+const app = new Vue({
+
+    el: '#app',
+    data: {
+        title: 'Hello VueJs',
+        image: 'https://cdn.pixabay.com/photo/2016/11/23/14/45/coding-1853305_1280.jpg'
+    }
+}) ;
